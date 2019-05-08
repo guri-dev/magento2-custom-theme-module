6d7f8306e1786bc31dcdd5ad116963f8a7bc27a7
Gurcharan singh <gurcharan@smartshore.nl>
2019-05-08 17:40:13 +0530

    fixed edit page saving values in slide edit page

--- Smile/Controller/Adminhtml/Slider/Save.php
+++ Smile/Controller/Adminhtml/Slider/Save.php
@@ -4,106 +4,85 @@ namespace Pilot\Smile\Controller\Adminhtml\Slider;
 
 use Pilot\Smile\Model\SliderFactory;
 use Pilot\Smile\Model\Slider;
+use Pilot\Smile\Api\SliderRepositoryInterface;
 use Magento\Framework\App\Filesystem\DirectoryList;
 use Magento\Framework\Filesystem;
 use Magento\Framework\App\Action\HttpPostActionInterface as HttpPostActionInterface;
+use Magento\Backend\App\Action;
+use Magento\Framework\App\Request\DataPersistorInterface;
+use Magento\Framework\Exception\LocalizedException;
 
 class Save extends \Magento\Backend\App\Action implements HttpPostActionInterface
 {	
-	protected $_fileUploaderFactory;
-	protected $uploaderFactory;
-	protected $filesystem;
-	protected $dataPersistor;
-	/**
-     * @var PostDataProcessor
-     */
-    protected $dataProcessor;
-
-    /**
-     * @var \Magento\Cms\Model\PageFactory
-     */
-    private $slideFactory;
-
-    /**
-     * @var \Magento\Cms\Api\PageRepositoryInterface
-     */
-    private $slideRepository;
-	
-
-	public function __construct(
+    protected $_dataSlider;
+    protected $_fileUploaderFactory;
+    protected $uploaderFactory;
+    protected $filesystem;
+    protected $dataPersistor;
+    private $sliderRepository;
+    
+    public function __construct(
         \Magento\Backend\App\Action\Context $context,
-		\Pilot\Smile\Model\SliderFactory  $slideFactory,
-		\Magento\Framework\Image\AdapterFactory $adapterFactory,
-		\Magento\Framework\Filesystem $filesystem,
-		\Magento\MediaStorage\Model\File\UploaderFactory $uploaderFactory,
-		\Magento\Framework\App\Request\DataPersistorInterface $dataPersistor,
-		\Pilot\Smile\Api\SliderRepositoryInterface $slideRepository = null
-	)
-	{
-		$this->uploaderFactory = $uploaderFactory;
-		$this->adapterFactory = $adapterFactory;
-		$this->_filesystem = $filesystem;
-		$this->dataPersistor = $dataPersistor;
+        \Pilot\Smile\Model\SliderFactory  $Slider,
+        \Magento\Framework\Image\AdapterFactory $adapterFactory,
+        \Magento\Framework\Filesystem $filesystem,
+        \Magento\MediaStorage\Model\File\UploaderFactory $uploaderFactory,
+        \Magento\Framework\App\Request\DataPersistorInterface $dataPersistor,
+        SliderRepositoryInterface $sliderRepository = null
+    )
+    {
+        $this->uploaderFactory = $uploaderFactory;
+        $this->adapterFactory = $adapterFactory;
+        $this->_filesystem = $filesystem;
+        $this->dataPersistor = $dataPersistor;
         parent::__construct($context);
-		$this->slideFactory = $slideFactory;
-		$this->slideRepository = $slideRepository
-            ?: \Magento\Framework\App\ObjectManager::getInstance()
-                ->get(\Pilot\Smile\Api\SliderRepositoryInterface::class);
-	}
+        $this->sliderRepository = $sliderRepository
+            ?: \Magento\Framework\App\ObjectManager::getInstance()->get(SliderRepositoryInterface::class);
+        $this->_dataSlider = $Slider;
+    }
 
 	public function execute()
-    {
-        $data = $this->getRequest()->getPostValue();
-        /** @var \Magento\Backend\Model\View\Result\Redirect $resultRedirect */
-        $resultRedirect = $this->resultRedirectFactory->create();
-        if ($data) {
-            //$data = $this->dataProcessor->filter($data);
-            if (isset($data['is_active']) && $data['is_active'] === 'true') {
-                $data['is_active'] = Page::STATUS_ENABLED;
+	{	
+        $model = $this->_dataSlider->create();
+        
+        $slide_id = $this->getRequest()->getParam('slide_id');
+        $slide_title = $this->getRequest()->getParam('slide_title');
+        $slide_description = $this->getRequest()->getParam('slide_description');
+        $slider_image = $this->getRequest()->getParam('slider_image');
+
+
+        if ($slide_id) {            
+            $collections = $this->_dataSlider->create()->getCollection()
+                 ->addFieldToFilter('slide_id', array('eq' => $slide_id));
+            foreach($collections as $item)
+            {
+                $item->setSlideTitle($slide_title);
+                $item->setSlideDescription($slide_description);
+                $item->setSlideImage(isset($slider_image[0]['url']) ? $slider_image[0]['url']:'');
             }
-            if (empty($data['page_id'])) {
-                $data['page_id'] = null;
+            $saveData = $collections->save();
+            if($saveData){
+                $this->messageManager->addSuccess( __('Record updated Successfully !') );
             }
-
-            /** @var \Magento\Cms\Model\Page $model */
-            $model = $this->slideFactory->create();
-
-            $id = $this->getRequest()->getParam('page_id');
-            if ($id) {
-                try {
-                    $model = $this->pageRepository->getById($id);
-                } catch (LocalizedException $e) {
-                    $this->messageManager->addErrorMessage(__('This page no longer exists.'));
-                    return $resultRedirect->setPath('*/*/');
-                }
-            }
-
-            $model->setData($data);
-
-            $this->_eventManager->dispatch(
-                'cms_page_prepare_save',
-                ['page' => $model, 'request' => $this->getRequest()]
-            );
-
-            // if (!$this->dataProcessor->validate($data)) {
-            //     return $resultRedirect->setPath('*/*/edit', ['page_id' => $model->getId(), '_current' => true]);
-            // }
-
-            try {
-                $this->pageRepository->save($model);
-                $this->messageManager->addSuccessMessage(__('You saved the slide.'));
-                return $this->processResultRedirect($model, $resultRedirect, $data);
-            } catch (LocalizedException $e) {
-                $this->messageManager->addExceptionMessage($e->getPrevious() ?: $e);
-            } catch (\Exception $e) {
-                $this->messageManager->addExceptionMessage($e, __('Something went wrong while saving the page.'));
-            }
-
-            $this->dataPersistor->set('cms_page', $data);
-            return $resultRedirect->setPath('*/*/edit', ['page_id' => $this->getRequest()->getParam('page_id')]);
+            $this->_redirect('pilot_smile/slider/index');
+            
         }
-        return $resultRedirect->setPath('*/*/');
-    }
+        else
+        {
+            
+            $model->addData([
+                "slide_title" => $slide_title,
+                "slide_description" => $slide_description,
+                "slide_image" => isset($slider_image[0]['url']) ? $slider_image[0]['url']:'',
+                ]);
+            $saveData = $model->save();
+            if($saveData){
+                $this->messageManager->addSuccess( __('Insert Record Successfully !') );
+            }
+            $this->_redirect('pilot_smile/slider/index');
+        }
+                
+	}
 
 
 	public function _filterFoodData(array $rawData)
