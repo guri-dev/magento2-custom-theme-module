121121249aa12ef2ed8080a5bc48d832125ad883
Gurcharan singh <gurcharan@smartshore.nl>
2019-05-09 17:55:02 +0530

    Added is_featured product Entity Attribute Value in catalog_product to show featured products on home page

--- Smile/Setup/InstallData.php
+++ Smile/Setup/InstallData.php
@@ -1,5 +1,8 @@
 <?php
 namespace Pilot\Smile\Setup;
+
+use Magento\Eav\Setup\EavSetup;
+use Magento\Eav\Setup\EavSetupFactory;
 use Magento\Framework\Setup\InstallDataInterface;
 use Magento\Framework\Setup\ModuleContextInterface;
 use Magento\Framework\Setup\ModuleDataSetupInterface;
@@ -7,16 +10,47 @@ class InstallData implements InstallDataInterface
 {
 	protected $_optionsFactory;
 	protected $_sliderFactory;
+	private $eavSetupFactory;
+
 	public function __construct(
 		\Pilot\Smile\Model\OptionsFactory $optionsFactory,
-		\Pilot\Smile\Model\SliderFactory $sliderFactory
+		\Pilot\Smile\Model\SliderFactory $sliderFactory,
+		EavSetupFactory $eavSetupFactory
 		)
 	{
 		$this->_optionsFactory = $optionsFactory;
 		$this->_sliderFactory = $sliderFactory;
+		$this->eavSetupFactory = $eavSetupFactory;
 	}
 	public function install(ModuleDataSetupInterface $setup, ModuleContextInterface $context)
 	{
+
+		$eavSetup = $this->eavSetupFactory->create(['setup' => $setup]);
+		$eavSetup->addAttribute(
+			\Magento\Catalog\Model\Product::ENTITY,
+			'is_featured',
+				[
+				'group' => 'General',
+				'type' => 'int',
+				'backend' => '',
+				'frontend' => '',
+				'label' => 'Featured Product',
+				'input' => 'boolean',
+				'class' => '',
+				'source' => '',
+				'global' => \Magento\Catalog\Model\ResourceModel\Eav\Attribute::SCOPE_GLOBAL,
+				'visible' => true,
+				'required' => false,
+				'user_defined' => true,
+				'default' => '',
+				'searchable' => false,
+				'filterable' => false,
+				'comparable' => false,
+				'visible_on_front' => false,
+				'used_in_product_listing' => true,
+				'unique' => false,
+				'apply_to' => 'simple,configurable,virtual,bundle,downloadable'
+				] );
 		
 		$data = [
 			'option_name' => "phone",
--- Smile/Setup/UpgradeData.php
+++ Smile/Setup/UpgradeData.php
@@ -1,21 +1,57 @@
 <?php
 namespace Pilot\Smile\Setup;
+
+use Magento\Eav\Setup\EavSetup;
+use Magento\Eav\Setup\EavSetupFactory;
 use Magento\Framework\Setup\UpgradeDataInterface;
 use Magento\Framework\Setup\ModuleDataSetupInterface;
 use Magento\Framework\Setup\ModuleContextInterface;
 class UpgradeData implements UpgradeDataInterface
 {
 	protected $_optionsFactory;
+	private $eavSetupFactory;
 	public function __construct(
 		\Pilot\Smile\Model\OptionsFactory $optionsFactory,
-		\Pilot\Smile\Model\SliderFactory $sliderFactory
+		\Pilot\Smile\Model\SliderFactory $sliderFactory,
+		EavSetupFactory $eavSetupFactory
 		)
 	{
 		$this->_optionsFactory = $optionsFactory;
 		$this->_sliderFactory = $sliderFactory;
+		$this->eavSetupFactory = $eavSetupFactory;
 	}
 	public function upgrade(ModuleDataSetupInterface $setup, ModuleContextInterface $context)
 	{
+		
+		$eavSetup = $this->eavSetupFactory->create(['setup' => $setup]);
+		$eavSetup->addAttribute(
+			\Magento\Catalog\Model\Product::ENTITY,
+			'is_featured',
+				[
+				'group' => 'General',
+				'type' => 'int',
+				'backend' => '',
+				'frontend' => '',
+				'label' => 'Featured Product',
+				'input' => 'boolean',
+				'class' => '',
+				'source' => '',
+				'global' => \Magento\Catalog\Model\ResourceModel\Eav\Attribute::SCOPE_GLOBAL,
+				'visible' => true,
+				'required' => false,
+				'user_defined' => true,
+				'default' => '',
+				'searchable' => false,
+				'filterable' => false,
+				'comparable' => false,
+				'visible_on_front' => false,
+				'used_in_product_listing' => true,
+				'unique' => false,
+				'apply_to' => 'simple,configurable,virtual,bundle,downloadable'
+				] );
+		
+		
+		
 		if (version_compare($context->getVersion(), '1.2.0', '<')) {
 			$data = [
 				'option_name' => "phone",
