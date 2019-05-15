1268f4f8531994f73f695ec2b5d7a6b5cb51e35c
Gurcharan singh <gurcharan@smartshore.nl>
2019-05-15 18:56:38 +0530

    UI for theme options on admin end

--- Smile/Block/Options.php
+++ Smile/Block/Options.php
@@ -13,13 +13,30 @@ class Options extends \Magento\Backend\Block\Widget
      *
      * @var \Magento\Config\Model\Config\Structure\Element\Iterator
      */
-    protected $_tabs;
+    protected $optionsFactory;
 
     public function __construct(
         \Magento\Backend\Block\Template\Context $context,
-        array $data = []
+        array $data = [],
+        \Pilot\Smile\Model\OptionsFactory  $options
     ) {
         parent::__construct($context, $data);
+        $this->optionsFactory = $options;
+    }
+
+    public function getOptions()
+    {
+        $optionsModel = $this->optionsFactory->create();
+        $optionsList = $optionsModel->getCollection();
+        if(!empty($optionsList->getData()))
+        {
+                return $optionsList->getData();
+        }
+        else
+        {
+                return "no options available";
+        }
+        
     }
    
     
